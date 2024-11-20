f5cfa03d4d6f45182af4b73b26c4f743e154389a
Elise <chen.elise17@gmail.com>
2024-11-19 19:19:37 -0800

    Alter User and Event Models

--- dbapp/models.py
+++ dbapp/models.py
@@ -6,18 +6,26 @@ class User(models.Model):
         ('Admin', 'Admin')
     ]
 
-    first_name = models.CharField(max_length=50, null=True, blank=True)
-    last_name = models.CharField(max_length=50, null=True, blank=True)
+    DEPARTMENT_CHOICES = [
+        ('Khoury', 'Khoury'),
+        ('COE', 'COE')
+    ]
+
+
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
     email = models.EmailField(primary_key=True)
     password = models.CharField(max_length=20)
     role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='Student')
     created_at = models.DateTimeField(auto_now_add=True)
+    department = models.CharField(max_length=10, choices=DEPARTMENT_CHOICES, default='Khoury')
 
     def __str__(self):
         return self.email
 
 
 class Event(models.Model):
+    event_id = models.AutoField(primary_key=True)
     event_name = models.CharField(max_length=255)
     event_description = models.TextField()
     event_location = models.CharField(max_length=255)

2ddbb123c78cdffd707c55d09a47c4df2df392e3
Elise <chen.elise17@gmail.com>
2024-11-24 20:01:37 -0800

    Add chatbot api

--- dbapp/urls.py
+++ dbapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import SignUpView, SignInView, PublishEventView, ListEventView, FilterEventByCategoryView, UpdateEventStatusView
+from .views import SignUpView, SignInView, PublishEventView, ListEventView, FilterEventByCategoryView, UpdateEventStatusView, GenerateSQLView, ExecuteSQLView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
@@ -8,4 +8,6 @@ urlpatterns = [
     path('list-events/', ListEventView.as_view(), name='list_events'),
     path('filter-events/', FilterEventByCategoryView.as_view(), name='filter_events'),
     path('update-event-status/<int:event_id>/status', UpdateEventStatusView.as_view(), name='update_event_status'),
+    path('generate-sql/', GenerateSQLView.as_view(), name='generate_sql'),
+    path('execute-sql/', ExecuteSQLView.as_view(), name='execute_sql'),
 ]
